bab9f5deea3c16f56dec172f750d1dc22af415f2
Maxim <maxim.yumakov@gmail.com>
2022-04-28 22:35:43 +0400

    Create abstract class

--- Yumakov.atm/Program.cs
+++ Yumakov.atm/Program.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace Yumakov.atm
 {
+    abstract class Score
+    {
+        protected int score;
+        protected abstract string ShowScore();
+    }
     class Program
     {
         static void Main(string[] args)
